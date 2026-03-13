f1f89dacfbd2173ea372a80c181b6e7860067147
Gribbariga <mormir09@gmail.com>
2026-03-13 19:18:51 +0500

    login form layout

--- aitiguru/src/features/LoginForm/ui/LoginForm.tsx
+++ aitiguru/src/features/LoginForm/ui/LoginForm.tsx
@@ -1,7 +1,25 @@
+import { Button } from '@shared/UI/Button/Button';
+import { Checkbox } from '@shared/UI/Checkbox/Checkbox';
+import { Input } from '@shared/UI/Input/Input';
+
+import styles from './LoginForm.module.scss';
+
 export const LoginForm = () => {
   return (
-    <>
-      <></>
-    </>
+    <form className={styles.form}>
+      <div className={styles.inputsWrapper}>
+        <Input label="Логин" placeholder="Логин" />
+        <Input label="Пароль" placeholder="Пароль" />
+      </div>
+
+      <Checkbox
+        label={'Запомнить данные'}
+        checked={false}
+        onChange={function (checked: boolean): void {
+          throw new Error('Function not implemented.');
+        }}
+      />
+      <Button className={styles.button}>Войти</Button>
+    </form>
   );
 };
