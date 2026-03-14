b92b95a102bbde046cd9d5bf81c71b5ab212660c
Gribbariga <mormir09@gmail.com>
2026-03-14 05:07:44 +0500

    add add to cart

---
+++ aitiguru/src/features/AddToCart/UI/AddToCart.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@shared/UI/Button/Button';
+
+import Plus from '../assets/PlusIcon.svg?react';
+
+export const AddToCart = () => {
+  return (
+    <Button styleType="small">
+      <Plus />
+    </Button>
+  );
+};
